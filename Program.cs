3bacef0ceaa53a9a5226a7c6263b63c5370c18cc
Eugen Karaman <karamanjekakol@gmail.com>
2024-06-25 16:52:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _28._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "notepad.exe";
+                process.StartInfo.Arguments = "";
+
+                process.Start();
+
+                process.WaitForExit();
+
+                int exitCode = process.ExitCode;
+                Console.WriteLine($"Exit code: {exitCode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!!!");
+            }
+        }
+    }
+}
